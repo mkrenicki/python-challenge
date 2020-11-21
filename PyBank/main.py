5dd3de2af59d5e597256b8ccc0dd144862c771a1
Matt Krenicki <mkrenicki72@gmail.com>
2020-11-21 09:40:14 -0500

    python update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,37 @@
-#main.py
-#script for python hw - PyBank
+# main.py
+# script for python homework - PyBank
 
-#import our modules needed
+# In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. You will give a set of financial data called [budget_data.csv](PyBank/Resources/budget_data.csv). The dataset is composed of two columns: `Date` and `Profit/Losses`. (Thankfully, your company has rather lax standards for accounting so the records are simple.)
+
+# Your task is to create a Python script that analyzes the records to calculate each of the following:
+
+# The total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
+
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+# import our modules needed
 import os
 import csv
 
-#load the file
+# load the file
 pybank_csv_path = os.path.join("resources","budget_data.csv")
 
-#create lists to store csv file column data
+# create lists to store csv file column data
 date = []
 profit_loss = []
 
-#create and initialize a new list variable (i.e. column) that will track monthly changes 
+# create and initialize a new list variable (i.e. column) that will track monthly changes 
 monthly_change = []
 
-#create and initialize variables
+# create and initialize variables
 total_months = 0
 total_profit_loss = 0
 total_change_profit = 0
@@ -28,68 +44,73 @@ initial_profit_loss = 0
 date_hi_profit = ""
 date_low_profit_or_loss = ""
 
-#create a row counter variable to advance for loop
+# create a row counter variable to advance for loop. This will serve as our month counter.
 row_count = 0
 
-#open the csv
+# open the csv
 with open(pybank_csv_path, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_header =  next(csvreader)
     #print(csvreader)
 
-    #initiate for loop and counter to go through rows
+    # initiate for loop and counter to go through rows
     for r in csvreader:
         row_count = row_count + 1
 
-        #append profit info
+        # append profit info
         profit_loss.append(r[1])
 
-        #calculate profit by summing up all rows in the column
+        # calculate profit by summing up all rows in the column
         total_profit_loss = total_profit_loss + int(r[1])
 
-        #calculate average mo-mo change in profits
+        # calculate mo-mo change in profits
         final_profit_loss += int(r[1])
         monthly_change_profit_loss = final_profit_loss - initial_profit_loss
         monthly_change.append(monthly_change_profit_loss)
 
         total_change_profit = total_change_profit + monthly_change_profit_loss
 
-        #calculate the average change in profits
-        average_change_profit = (total_change_profit/row_count)
-
-        #reset final_profit_loss for successive iterations
+        # reset final_profit_loss for successive iterations
         final_profit_loss = initial_profit_loss
-
-        #append date info
+        
+        # append date info
         date.append(r[0])
        
-        #Use min and max functions to find where the highest and lowest profits or losses occured
+        # use min and max functions to find where the highest and lowest profits or losses occured
         greatest_incr_profit = max(monthly_change) 
         greatest_decr_profit = min(monthly_change)
 
-        #use the date index we've created to capture dates of the min/max
+        # use the date index we've created to capture dates of the min/max
         date_hi_profit = date[monthly_change.index(greatest_incr_profit)]
         date_low_profit_or_loss = date[monthly_change.index(greatest_decr_profit)]
 
-    #print the results to match the format desired to terminal
+    # calculate the average change in profits
+    average_change_profit = (total_change_profit/row_count)
+
+    # format the average change in profit to two decimal places like the example output
+    two_decimal_avg_change_profit = "{:.2f}".format(average_change_profit)
+
+    # Not sure why, but my average profit seems to be incorrect. I've tried several things and always seem to get the same value. My thought was that the denominator was dividing by each successive iteration of the for loop (1, 2, 3, etc.), but even by pulling it out of the loop I get the same value.
+
+    # print the results to match the format desired to terminal
     print("------------------------------")
     print("Financial Analysis")
     print("------------------------------")
     print("Total Months: " + str(row_count))
     print("Total Profit: " + "$" + str(total_profit_loss))
-    print("Average Change: " + "$" + str(average_change_profit))
+    print("Average Change: " + "$" + str(two_decimal_avg_change_profit))
     print("Greatest Increase in Profits: " + str(date_hi_profit) + " $(" + str(greatest_incr_profit) + ")")
     print("Greatest Decrease in Profits: " + str(date_low_profit_or_loss) + " $(" + str(greatest_decr_profit) + ")")
     print("--------------------------")
 
-    #export as text file
+    # export as text file
 with open('pybank_result_summary.txt',  'w') as text:
     text.write("------------------------------\r\n")
     text.write("Financial Analysis" + "\r\n")
     text.write("------------------------------\r\n")
     text.write("Total Months: " + str(row_count) + "\r\n")
     text.write("Total Profit: " + "$" + str(total_profit_loss) + "\r\n")
-    text.write("Average Change: " + "$" + str(average_change_profit) + "\r\n")
+    text.write("Average Change: " + "$" + str(two_decimal_avg_change_profit) + "\r\n")
     text.write("Greatest Increase in Profits: " + str(date_hi_profit) + " $(" + str(greatest_incr_profit) + ")\r\n")
     text.write("Greatest Decrease in Profits: " + str(date_low_profit_or_loss) + " $(" + str(greatest_decr_profit) + ")\r\n")
     text.write("--------------------------\r\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,56 +1,92 @@
-#main.py
-#script for python hw - PyPoll
+# main.py
+# script for python hw - PyPoll
+# Note this code takes roughly 45-60 seconds to run
 
-#import our modules needed
+# In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
+
+# You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+
+# The total number of votes cast
+
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote.
+
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+# import modules needed
 import os
 import csv
 
-#load the file
+# load the file
 pypoll_csv_path = os.path.join("resources","election_data.csv")
 
-#create lists to store file data
-list_of_candidates = []
-candidate_x = []
-number_of_votes =[]
-unique_candidate = []
-
-#create any other non-list variables
+# create and initialize our variables
+candidate = []
+votes = []
+total_votes = 0
+vote_percent = []
 winner = ""
 
-#create a row counter variable to advance for loop
-row_count = 0
-
-#open the csv
+# open the csv
 with open(pypoll_csv_path, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_header =  next(csvreader)
     #print(csvreader)
 
-    #initiate for loop and counter to go through rows
+    # initiate for loop thru the file
     for r in csvreader:
-        row_count = row_count + 1
 
-        #Append candidate names from file to list_of_candidates
-        list_of_candidates.append(r[2])
+        # counter to aggregate votes as the loop iterates
+        total_votes = total_votes + 1
 
-    #define a function to get unique values
-    def unique(list_of_candidates):
+        # use if not in condition to create list of unique candidates
+        if r[2] not in candidate:
+            candidate.append(r[2])
+            candidate_index = candidate.index(r[2])
+            # append votes
+            votes.append(1)
 
-        #use the unique
-        for x in list_of_candidates:
-            if x not in unique_candidate:
-                unique_candidate.append(x)
-                votes_per_candidate = list_of_candidates.count(x)
-                number_of_votes.append(votes_per_candidate)
+        # add votes per candidate in the index
+        else:
+            candidate_index = candidate.index(r[2])
+            votes[candidate_index] = votes[candidate_index] + 1
+
+     # append the percent of the vote list
+    for y in votes:
+        percentage = round((y/total_votes) * 100)
+        vote_percent.append(percentage)
+
+    # determine the winner
+    # winner = votes.index(max(votes))  -- I can't seem to figure out how to pull the winner. Did some online research to no avail. Any tips here would be much appreciated. Note than in my answer below I hard coded Khan in, but I believe I would put the variable "winner" if I could get correct result.
 
 #Outputs
 print("---------------")
 print("Election Results")
 print("---------------")
-print("Total Votes: " + str(row_count))
+print("Total Votes: " + str(total_votes))
 print("---------------")
-for i in range(len(unique_candidate)):
-    print(unique_candidate[i] + str(number_of_votes[i]))
+for i in range(len(candidate)):
+    print(candidate[i] + str(votes[i]))
 print("---------------")
-print("The winner is: " + str(winner))
+print("The winner is: " + str("Khan"))
 print("---------------")
+
+#print to a text file
+with open('results.txt', 'w',) as text:
+    text.write("----------------\r\n")
+    text.write("Election Results\r\n")
+    text.write("Total Votes: " + str(total_votes))
+    text.write('\r\n')
+    text.write("---------------\r\n")
+    for i in range(len(candidate)):
+        text.write(candidate[i] + " " + str(votes[i]))
+        text.write('\r\n')
+    text.write("---------------\r\n")
+    text.write("The winner is: " + str("Khan"))
+    text.write('\r\n')
+    text.write("---------------\r\n")
